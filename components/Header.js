8a453a3054dca3bab66abda31e8c9c535bf34080
kahasan <kahraman.h@outlook.com>
2020-01-31 16:45:18 +0300

    Header tasarım değişiklikleri

--- components/Header.js
+++ components/Header.js
@@ -11,38 +11,44 @@ function Header() {
           href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css?family=Cormorant+Garamond&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <div className="container">
         <div className="hero">
           <div className="row align-items-center">
-            <h1 className="hero-title col-md-6">
+            <h1 className="hero-title col-xl-6">
               <Link href="/">
-                <a style={{ color: 'black' }}>HASANKA</a>
+                <a>HASANKA</a>
               </Link>
             </h1>
 
-            <div className="social-links col-md-6">
-              <span className="welcome social-link">Hoşgeldiniz</span>
+            <div className="social-links col-xl-6">
+              <span className="welcome social-link">
+                Kişisel Blog'uma Hoşgeldiniz
+              </span>
               <a
                 href="https://www.twitter.com/Hsn_Kahramn"
                 className="social-link"
                 target="_blank"
               >
-                <img src="../icons/twitter.svg" className="icon"></img>
+                <img src="../icons/twitter.png" className="icon"></img>
               </a>
               <a
                 href="https://www.linkedin.com/in/hasan-kahraman-790289111/"
                 className="social-link"
                 target="_blank"
               >
-                <img src="../icons/linkedin.svg" className="icon"></img>
+                <img src="../icons/linkedin.png" className="icon"></img>
               </a>
               <a
                 href="https://www.instagram.com/hsnkahraman/"
                 className="social-link"
                 target="_blank"
               >
-                <img src="../icons/instagram.svg" className="icon"></img>
+                <img src="../icons/instagram.png" className="icon"></img>
               </a>
             </div>
           </div>
